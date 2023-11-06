65ec07850cb3fb1b7bb4f86b420e0b2b77aa1c87
Achref Dahmen <willandahmen@gmail.com>
2023-11-06 18:17:57 +0100

    ip

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://192.168.159.130:8082';
+  private apiUrl = 'http://192.168.23.128:8082';
 
   constructor(private http: HttpClient) { }
 
